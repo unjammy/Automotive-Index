bfcf4369eabe469176739c43a46ac3879d605bc0
clarkdenunzio <clark@clarkdenunzio.com>
2017-07-22 18:46:04 -1000

    listed all the lambos

--- indexing.sql
+++ indexing.sql
@@ -1,5 +1,5 @@
-DROP USER IF EXISTS indexed_cars_user;
-DROP DATABASE IF EXISTS indexed_cars;
+DROP USER indexed_cars_user;
+DROP DATABASE indexed_cars;
 
 CREATE USER indexed_cars_user;
 CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
@@ -8,15 +8,24 @@ CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
 
 \i scripts/car_models.sql
 
-\i scripts/car_models_data.spl
-\i scripts/car_models_data.spl
-\i scripts/car_models_data.spl
-\i scripts/car_models_data.spl
-\i scripts/car_models_data.spl
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
 
-\i scripts/car_models_data.spl
-\i scripts/car_models_data.spl
-\i scripts/car_models_data.spl
-\i scripts/car_models_data.spl
-\i scripts/car_models_data.spl
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
 
+-- Timing #1 = 35.759 ms
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
+
+-- Timing #1 = 26.829 ms
+SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+
+-- Timing #1 =
+\timing
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
