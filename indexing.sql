827c136628037f165b6c36c2a354581d060227b6
clarkdenunzio <clark@clarkdenunzio.com>
2017-07-22 18:51:16 -1000

    slow queries are done

--- indexing.sql
+++ indexing.sql
@@ -26,6 +26,12 @@ SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
 -- Timing #1 = 26.829 ms
 SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
 
--- Timing #1 =
-\timing
+-- Timing #1 = 23.874 ms
 SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+
+-- Timing #1 = 79.241 ms
+SELECT * FROM car_models WHERE year <= 2015 AND year >= 2010;
+
+-- Timing #1 = 34.550 ms
+SELECT * FROM car_models WHERE year = 2010;
+
